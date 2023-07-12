70a57e68e79a2761d6376425b87a8c9f46a2d0a1
bksooon <sbka0704@nate.com>
2023-07-12 16:25:26 +0900

    생성

---
+++ hello.java
@@ -0,0 +1,13 @@
+// public class Hello
+// {
+// 	public static void main(String[] args){
+// 		System.out.println("Hello World");
+// 	}
+// }
+// 파일 이름: Hello.java
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
